25753cc6fbe635c2ff252ed615121306e811b79d
Kristopher Kalish <kris@kalish.net>
2015-12-06 00:40:28 -0500

    set up project structure

---
+++ hologram-service/src/main/java/net/kalish/net/hologram/service/Main.java
@@ -0,0 +1,10 @@
+package net.kalish.net.hologram.service;
+
+/**
+ * Created by Kris on 12/6/2015.
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+    }
+}
---
+++ hologram-service/src/main/java/net/kalish/net/hologram/service/TcpConnector.java
@@ -0,0 +1,7 @@
+package net.kalish.net.hologram.service;
+
+/**
+ * Created by Kris on 12/6/2015.
+ */
+public class TcpConnector {
+}
